25e9fe72ae4e9f31deb16239352656813def3baf
jeanmarcandre <93780722+jeanmarcandre@users.noreply.github.com>
2022-05-03 12:15:29 +0200

    commit du 03/05

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,19 +10,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/post')]
-class PostController extends AbstractController
+#[Route('/blog', name: 'app_blog_')]
+class BlogController extends AbstractController
 {
-    #[Route('/', name: 'app_post_index', methods: ['GET'])]
-    public function index(PostRepository $postRepository): Response
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function indexBlog(PostRepository $postRepository): Response
     {
-        return $this->render('post/index.html.twig', [
+        return $this->render('blog/index.html.twig', [
             'posts' => $postRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_post_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, PostRepository $postRepository): Response
+    #[Route('/publier', name: 'new_post', methods: ['GET', 'POST'])]
+    public function newPost(Request $request, PostRepository $postRepository): Response
     {
         $post = new Post();
         $form = $this->createForm(PostType::class, $post);
@@ -34,25 +34,25 @@ class PostController extends AbstractController
 
             $this->addFlash('success', 'Publication ajoutée');
 
-            return $this->redirectToRoute('app_post_index');
+            return $this->redirectToRoute('app_blog_index');
         }
 
-        return $this->renderForm('post/new.html.twig', [
+        return $this->renderForm('blog/new.post.html.twig', [
             'post' => $post,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_post_show', methods: ['GET'])]
-    public function show(Post $post): Response
+    #[Route('/{id}', name: 'show_post', methods: ['GET'])]
+    public function showPost(Post $post): Response
     {
-        return $this->render('post/show.html.twig', [
+        return $this->render('blog/show.post.html.twig', [
             'post' => $post,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_post_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Post $post, PostRepository $postRepository): Response
+    #[Route('/{id}/modifier-publication', name: 'edit_post', methods: ['GET', 'POST'])]
+    public function editPost(Request $request, Post $post, PostRepository $postRepository): Response
     {
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
@@ -63,17 +63,17 @@ class PostController extends AbstractController
 
             $this->addFlash('success', 'Publication bien modifiée');
 
-            return $this->redirectToRoute('app_post_index');
+            return $this->redirectToRoute('app_blog_index');
         }
 
-        return $this->renderForm('post/edit.html.twig', [
+        return $this->renderForm('blog/edit.post.html.twig', [
             'post' => $post,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_post_delete', methods: ['POST'])]
-    public function delete(Request $request, Post $post, PostRepository $postRepository): Response
+    #[Route('/{id}', name: 'delete_post', methods: ['POST'])]
+    public function deletePost(Request $request, Post $post, PostRepository $postRepository): Response
     {
         if ($this->isCsrfTokenValid('transhepate_blog'.$post->getId(), $request->request->get('_token'))) {
             $postRepository->remove($post);
@@ -81,6 +81,6 @@ class PostController extends AbstractController
             $this->addFlash('success', 'Publication supprimée');
         }
 
-        return $this->redirectToRoute('app_post_index');
+        return $this->redirectToRoute('app_blog_index');
     }
 }
