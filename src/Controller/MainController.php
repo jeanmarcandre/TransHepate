901b4a441ed658aa1b7d30b9c1b12bdd07f641ff
jeanmarcandre <93780722+jeanmarcandre@users.noreply.github.com>
2022-05-04 16:02:05 +0200

    commit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Form\PostFormType;
 use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Loader\Configurator\twig;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 #[Route('/', name: 'app_main_')]
 class MainController extends AbstractController
@@ -33,5 +35,26 @@ class MainController extends AbstractController
     {
         // Cette page appellera la vue template/main/transhepatebfc.html.twig
         return $this->render('main/transhepatebfc.html.twig');
+    }
+
+    #[Route(path: '/connexion', name: 'login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_main_home');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/deconnexion', name: 'logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
