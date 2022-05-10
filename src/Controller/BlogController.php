ed30ed785f0998c5ee32ba2adb605fa4dc318297
jeanmarcandre <93780722+jeanmarcandre@users.noreply.github.com>
2022-05-10 14:25:15 +0200

    commit

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,15 +4,19 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\PostType;
+use App\Controller\BlogController;
 use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/blog', name: 'app_blog_')]
 class BlogController extends AbstractController
 {
+
+    /***********  Gestion des POSTS  *************/
+
     #[Route('/', name: 'index', methods: ['GET'])]
     public function indexBlog(PostRepository $postRepository): Response
     {
